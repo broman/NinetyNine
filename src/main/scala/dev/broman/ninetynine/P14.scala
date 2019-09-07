f85f1b00a0cb3aa29cbd3e79c6aa4b3ce967d05d
broman <ryanjbroman@gmail.com>
2019-09-06 23:44:41 -0700

    P15

--- src/main/scala/dev/broman/ninetynine/P14.scala
+++ src/main/scala/dev/broman/ninetynine/P14.scala
@@ -1,8 +1,8 @@
 package dev.broman.ninetynine
 
 // P14
-// Duplicate the elements in a list a given number of times
-// duplicate(2, List(1, 2, 3)
+// Duplicate the elements in a list.
+// duplicate(List(1, 2, 3)
 // => List(1, 1, 2, 2, 3, 3)
 
 object P14 {
---
+++ src/main/scala/dev/broman/ninetynine/P15.scala
@@ -0,0 +1,5 @@
+package dev.broman.ninetynine
+
+object P15 {
+
+}
