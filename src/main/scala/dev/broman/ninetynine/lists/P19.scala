9d7dae1c22edc2596cb96d7fe54512523d7b1c3f
broman <ryanjbroman@gmail.com>
2019-09-09 18:19:50 -0700

    P19

---
+++ src/main/scala/dev/broman/ninetynine/lists/P19.scala
@@ -0,0 +1,16 @@
+package dev.broman.ninetynine.lists
+
+// P19
+// Remove the Nth element in a list, returning a tuple of the new list and the removed element
+// remove(4, List(1, 2, 3, 4, 5, 6, 7)
+// => (List(1, 2, 3, 5, 6, 7), 5)
+
+object P19 {
+  def remove[A](n: Int, list: List[A]): (List[A], A) = {
+    list.splitAt(n) match {
+      case (Nil, _) if n < 0 => null
+      case (pre, end :: post)  => (pre ::: post, end)
+      case (_, Nil) => null
+    }
+  }
+}
